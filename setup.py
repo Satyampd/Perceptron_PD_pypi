70d61a9612bdd8947ac32ccabbf7fa9fe52c1607
Satyam Prasad Tiwari <satyamprasadtiwari@gmail.com>
2022-02-12 11:54:28 +0530

    updated src and version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "Satyampd"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}",
-    version="0.0.2",
+    version="0.0.3",
     author=USER_NAME,
     author_email="tiwarisatyampd@gmail.com",
     description="DESCRIPTION HERE",
